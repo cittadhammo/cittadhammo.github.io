5e5b35c5527d541271c50fe422d7dd06b6cd66e8
Cittadhammo <82962905+cittadhammo@users.noreply.github.com>
2025-03-13 07:44:47 +0000

    Quartz sync: Mar 13, 2025, 7:44 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -32,8 +32,8 @@ export const defaultContentPageLayout: PageLayout = {
     }),
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
+    Component.Graph(),
     Component.Backlinks(),
   ],
 }
