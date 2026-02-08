66d296cedc0065cf2b9b011e67280af960f567fe
Cittadhammo Bhikkhu <cittadhammobhikkhu@gmail.com>
2026-02-08 18:56:19 +0000

    new behaviour for map fullscreen on images
    need hover effect on button fullscreen

--- assets/scripts/lightbox.js
+++ assets/scripts/lightbox.js
@@ -4,11 +4,27 @@ $(document).ready(function() {
     type: 'image',
     gallery: {
       enabled: true,
-      navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, 1 - after current
+      navigateByImgClick: true, // Re-enable default image click navigation
+      preload: [0,1]
     },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+    callbacks: {
+      change: function() {
+        var mfpImg = this.content.find('.mfp-img');
+        mfpImg.off('click.mfpMap'); // Always remove previous custom click handlers
+      },
+      afterChange: function() {
+        // Always remove previous icon before adding a new one
+        this.content.find('.fullscreen-map-icon-in-lightbox').remove();
+        
+        var mapUrl = this.currItem.el.attr('data-map-url');
+        if (mapUrl) {
+          var fullscreenIcon = $('<a class="fullscreen-map-icon-in-lightbox" href="' + mapUrl + '"></a>');
+          fullscreenIcon.append('<img src="/assets/icons/fs300.png" alt="View Fullscreen Map" style="width: 159px; height: 32px;">');
+          
+          // Append to the <figure> element
+          this.content.find('figure').append(fullscreenIcon);
+        }
+      }
     }
   });
 });
