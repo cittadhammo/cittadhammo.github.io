0a9f7de24f74b00a5efd5f43674d50f1bdfd0d2e
Cittadhammo Bhikkhu <cittadhammobhikkhu@gmail.com>
2026-03-22 09:40:33 +0000

    Simplify lightbox trigger: click gallery image directly instead of rebuilding items

--- assets/scripts/lightbox.js
+++ assets/scripts/lightbox.js
@@ -43,50 +43,10 @@ $(document).ready(function() {
     e.preventDefault();
     var targetIndex = parseInt($(this).data('lightbox-index'), 10);
     if (!isNaN(targetIndex)) {
-      var $allLinks = $gallery.find('a.mfp-image');
-      var items = [];
-      $allLinks.each(function() {
-        items.push({
-          src: $(this).attr('href'),
-          type: 'image',
-          title: $(this).data('title') || ''
-        });
-      });
-      
-      $.magnificPopup.open({
-        items: items,
-        gallery: {
-          enabled: true
-        },
-        type: 'image',
-        image: {
-          titleSrc: function(item) {
-            return item.el.attr('data-title') || '';
-          }
-        },
-        callbacks: {
-          afterChange: function() {
-            this.content.find('.fullscreen-map-icon-in-lightbox').remove();
-            var mapUrl = $allLinks.eq(this.currIndex).attr('data-map-url');
-            var mfpImg = this.content.find('.mfp-img');
-            var siteBaseurl = (window.SITE && window.SITE.baseurl) ? window.SITE.baseurl : '';
-            siteBaseurl = siteBaseurl.replace(/\/+$/, '');
-
-            mfpImg.off('click').on('click.mfpMap', function(e) {
-              e.preventDefault();
-              e.stopPropagation();
-            }).css('cursor', 'default');
-
-            if (mapUrl) {
-              var fullscreenIcon = $('<a class="fullscreen-map-icon-in-lightbox" href="' + mapUrl + '" style="display: inline-block; top: 40px; width: 300px; height: 80px;"></a>');
-              var iconImage = $(`<img src="${siteBaseurl}/assets/icons/fs-off.png" alt="View Fullscreen Map" style="height: 100%; width: 100%; object-fit: contain; object-position: center;">`);
-              
-              fullscreenIcon.append(iconImage);
-              this.content.find('figure').append(fullscreenIcon);
-            }
-          }
-        }
-      }, targetIndex);
+      var $targetLink = $gallery.find('a.mfp-image').eq(targetIndex);
+      if ($targetLink.length) {
+        $targetLink.trigger('click');
+      }
     }
   });
 });
